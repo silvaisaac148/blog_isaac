666c179414295d0f7657252ba9e609fa61515145
silvaisaac148 <silvaisaac148@gmail.com>
2026-03-23 17:06:21 -0400

    fix: serve media files

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,10 +3,10 @@ URL configuration for the blog project.
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
+from django.views.static import serve
 from django.conf.urls.static import static
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -14,6 +14,7 @@ urlpatterns = [
     path("ckeditor5/", include("django_ckeditor_5.urls")),
 ]
 
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Servir archivos multimedia en producción (OJO: Railway requiere Volumen)
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
